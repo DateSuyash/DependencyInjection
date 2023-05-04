958ce31f45fa0e85edf25966ab446d130839a4e3
SUYASH DATE <datesuyash143@gmail.com>
2023-05-04 18:58:57 +0530

    Add project files.

---
+++ DependencyInjection/Services/DIService.cs
@@ -0,0 +1,15 @@
+﻿namespace DependencyInjection.Services
+{
+    public class DIService : ITransientService, IScopedService, ISingletonService
+    {
+        readonly Guid id;
+        public DIService()
+        {
+            this.id = Guid.NewGuid();
+        }
+        public Guid GetID()
+        {
+            return id;
+        }
+    }
+}
